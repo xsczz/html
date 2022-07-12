3d6f766fa648e298a6b5118abcb68942654d2f2c
陈朔 <seanschen@tencent.com>
2022-07-12 13:31:48 +0800

    add http request

---
+++ login2/lib/request.js
@@ -0,0 +1,45 @@
+class Request {
+
+    get(url, params = {}, successCallback, failCallback) {
+    
+        var query = ''
+    
+        var i = 0
+        for (var k in params) {
+            if (i == 0) {
+                query = '?' + k + '=' + params[k]
+            } else {
+                query += '&' + k + '=' + params[k]
+            }
+            i++
+        }
+    
+        url = url + query
+        var xhr = new XMLHttpRequest()
+    
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                successCallback(xhr.responseText)
+            }
+        }
+    
+        xhr.open('GET', url, true)
+        xhr.send()
+    }
+    
+    post(url, params = {}, successCallback, failCallback) {
+        var xhr = new XMLHttpRequest()
+    
+        xhr.open('POST', url, true)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        var json = JSON.stringify(params)
+    
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                successCallback(xhr.responseText)
+            }
+        }
+    
+        xhr.send(json)
+    }
+}
